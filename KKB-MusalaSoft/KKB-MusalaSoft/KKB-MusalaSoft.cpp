22093f49ecca8c542ba696a7b51d9af1772ee6f6
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2021-02-08 19:46:31 +0200

    Setup functions *Readint *centerStrings(center text in the middle of the screen based on screen size) * PrintStrings devides the string by searching for delimiter

--- KKB-MusalaSoft/KKB-MusalaSoft/KKB-MusalaSoft.cpp
+++ KKB-MusalaSoft/KKB-MusalaSoft/KKB-MusalaSoft.cpp
@@ -1,11 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <Windows.h>
+#include <cmath>
+
 using namespace std;
 
+int readInt()
+{
+    int a;
+
+    //loops until the user enters an integer
+    while (!(cin >> a))
+    {
+        cin.clear();
+        cin.ignore(INT_MAX, '\n');
+        cout << "Incorrect value!" << endl;
+    }
+
+    return a;
+}
+
+void centerString(string s)
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+    //gets the window size and assigns it to the csbiInfo variable
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbiInfo);
+    COORD NewSBSize;
+    //assigns the x size of the screen
+    NewSBSize.X = csbiInfo.dwSize.X;
+    int l = s.size();
+    //calculates the spaces needed before the text in order to be centered
+    int pos = (int)((NewSBSize.X - l) / 2);
+    for (int i = 0; i < pos; i++)
+        cout << " ";
+
+    cout << s;
+}
+
+void printStrings(string rawStr)
+{
+    string str;
+    int indexDel = 0;
+    //searches for a comma in the string
+    while (rawStr.find(",") != string::npos)
+    {
+        indexDel = rawStr.find(",");
+        str = rawStr.substr(0, indexDel);
+        rawStr = rawStr.substr(indexDel + 1, rawStr.size() - indexDel);
+        centerString(str);
+        cout << endl;
+    }
+}
+
+bool menu() {
+
+}
 int main()
 {
-   
+    while (menu());
 }
 
 
