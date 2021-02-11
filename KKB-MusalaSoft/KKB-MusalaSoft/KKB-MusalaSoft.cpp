bb78e8fe7d2b2ebcdc3276be0de96f8189e8b90d
Valentin Petrov <VPPetrov18@codingburgas.bg>
2021-02-11 12:22:00 +0200

    Add menu *option 1 *option 8 *fixing the frontend

--- KKB-MusalaSoft/KKB-MusalaSoft/KKB-MusalaSoft.cpp
+++ KKB-MusalaSoft/KKB-MusalaSoft/KKB-MusalaSoft.cpp
@@ -14,55 +14,65 @@
 using namespace std;
 
 bool menu() {
-    cout << ".................................................." << endl;
-    cout << "1.Enter a new school." << endl;
-    cout << "2.Enter a new students in the list" << endl;
-    cout << "3.Enter a new teacher in the list" << endl;
-    cout << "4.Delete an already exist student from the list" << endl;
-    cout << "5.Delete an already exist teacher from the list" << endl;
-    cout << "6.Display a school data" << endl;
-    cout << "7.Go to reports menu" << endl; 
-    cout << "8.Alphabetical distribution" << endl;
-    cout << "9.Random distribution" << endl;
-    cout << ".................................................." << endl;
-    
+    cout << "......................................................." << endl;
+    cout << ". 1.Enter a new school.                               ." << endl;
+    cout << ". 2.Enter a new students in the list                  ." << endl;
+    cout << ". 3.Enter a new teacher in the list                   ." << endl;
+    cout << ". 4.Delete an already exist student from the list     ." << endl;
+    cout << ". 5.Delete an already exist teacher from the list     ." << endl;
+    cout << ". 6.Alphabetical distribution                         ." << endl;
+    cout << ". 7.Random distribution                               ." << endl;
+    cout << ". 8.Exit                                              ." << endl;
+    cout << "......................................................." << endl;
+
     cout << endl;
 
     int option;
     cout << "Please enter your option here -> ";
     cin >> option;
+    cout << endl;
+    if (option == 1)
+    {
+        SCHOOL sch = enterSchoolData();
+        cout << "School city: ";
+        cout << sch.city << endl;
+        cout << "School name: ";
+        cout << sch.name << endl;
+        cout << "School address: ";
+        cout << sch.address << endl;
+
+        cout << endl << "Students data: " << endl << endl;
+        printStudentsData(sch.students);
+        cout << "Teams data: " << endl << endl;
+        printTeamsData(sch.teams);
+        cout << "Teachers data: " << endl << endl;
+        printTeachersData(sch.teachers);
+
+        return true;
+    }
+    else if (option == 8)
+    {
+        return false;
+    }
 
 
     /*while (checkStudentsData(students) == false)
     {
         enterStudentsData(students);
     }*/
-    return false;
+
 
 }
- 
+
 
 int main()
 {
-    //while (menu());
-     
-    SCHOOL sch = enterSchoolData();
-    cout << "School city: ";
-    cout << sch.city << endl;
-    cout << "School name: ";
-    cout << sch.name << endl;
-    cout << "School address: ";
-    cout << sch.address << endl;
+    while (menu());
 
-    cout << endl << "Students data: " << endl << endl;
-    printStudentsData(sch.students);
-    cout << "Teams data: " << endl << endl;
-    printTeamsData(sch.teams);
-    cout << "Teachers data: " << endl << endl;
-    printTeachersData(sch.teachers);
 
-    
-    
+
+
+
 
 
     /*fstream studs, teachers, teams, schools;
@@ -78,3 +88,4 @@ int main()
     teams.close();
     schools.close();*/
 }
+
